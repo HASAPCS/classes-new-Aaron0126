60d13366c9ff22a1f23795d32218d963ea698e27
Aaron0126 <aaron.lin@has.hc.edu.tw>
2023-09-27 12:28:44 +0800

    done

--- src/main/java/com/example/Clock.java
+++ src/main/java/com/example/Clock.java
@@ -10,16 +10,22 @@ public class Clock {
         // TODO: Initialize the hours and minutes
     }
 
-    public void addMinutes(int minutes) {
-        this.minutes=minutes+minutes;
-        // if minutes>60;
-        
+    public void addMinutes(int minutesToAdd) {
+        this.minutes=minutes+=minutesToAdd;
+        if (this.minutes>=60){
+            this.hours+=1;
+            this.minutes-=60;
+        }
         // TODO: Add the value of the minutes parameter to the current minutes. 
         // Adjust the hours and minutes accordingly to represent a valid time.
     }
 
     public String getTime() {
+        if (this.hours>=13){
+            this.hours-=12;
+            return "0" + this.hours + ":" + this.minutes;
+        }
         // TODO: Return the time in the format "HH:MM"
-        return "0"; //this.hours, this.minutes;  // Change this default return statement
+        return this.hours + ":" + this.minutes; //this.hours, this.minutes;  // Change this default return statement
     }
 }
